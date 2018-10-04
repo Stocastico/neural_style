b715e8e2a720bcbd0bc37cf54acf3e67cfeea20a
Stefano Masneri <smasneri@vicomtech.org>
2018-10-04 16:21:03 +0200

    Add new files

---
+++ neural_style.py
@@ -0,0 +1,8 @@
+# Scientific Python
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+# Keras related
+from keras import backend as K
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg19 import VGG19
