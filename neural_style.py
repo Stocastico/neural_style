5ae511aa1c2ba85e5a0f467f1c6625e0ac8ec275
Stefano Masneri <smasneri@vicomtech.org>
2018-10-05 09:30:47 +0200

    Modify main script. Ignore pynb checkpoints

--- neural_style.py
+++
@@ -1,8 +0,0 @@
-# Scientific Python
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-# Keras related
-from keras import backend as K
-from keras.preprocessing.image import img_to_array
-from keras.applications.vgg19 import VGG19
